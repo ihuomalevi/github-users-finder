edfe979ad2715919ca49b15601d0ee0915384c3c
ihuomalevi <chibuikelevi19@gmail.com>
2020-06-07 06:10:05 -0700

    A mini-project where you can find github users by the letter starting their names

---
+++ app.js
@@ -0,0 +1,35 @@
+// Init GitHub
+const github = new Github;
+
+// Init uI
+const ui = new UI;
+
+// search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+    // Make http call
+   github.getUser(userText)
+   .then(data => {
+
+    if(data.profile.message === 'Not Found') {
+      // Show alert
+     ui.showAlert('User not found', 'alert alert-danger');
+
+    }else{
+     // Show profile
+    ui.showProfile(data.profile);
+    ui.showRepos(data.repos);
+    }
+
+   })
+  }else{
+    // clear profile
+    ui.clearProfile();
+  }
+});
---
+++ github.js
@@ -0,0 +1,21 @@
+class Github {
+  constructor() {
+    this.client_id = '7d5e206fd355c83f3e76';
+    this.client_secret = '5322d83536ec0d938859bdf254095bbd6484659a';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+  async getUser(user){
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos? per-page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return{
+      profile,
+      repos
+    }
+  }
+}
